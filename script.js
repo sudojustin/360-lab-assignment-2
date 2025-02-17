0f776336a8e883eaff131d29d93ca63c80698859
sudojustin <120149080+sudojustin@users.noreply.github.com>
2025-02-17 14:05:53 -0800

    Finished website

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentLocation = window.location.href;
     let menuLinks = document.querySelectorAll(".nav-link");
 
+    // First, remove active class from all links
+    menuLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+
+    // Then, add the active class to the link that matches the current location
     menuLinks.forEach(link => {
         if (link.href === currentLocation) {
             link.classList.add("active");
